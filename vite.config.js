d5797a80f501611e4dfb1401df980a3ae87081f8
coderisliev07 <coderbhai07@gmail.com>
2025-04-05 17:20:31 +0530

    deployment error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite';
+// vite.config.js
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'; // ✅ This is the correct import
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+});
